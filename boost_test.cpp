46b22d32b5abf3a1056e787676cf3b786b85945e
zhuanglewei <zhuanglewei@gmail.com>
2020-04-02 13:18:29 +0800

    slove include boost

---
+++ boost_test.cpp
@@ -0,0 +1,15 @@
+#define BOOST_TEST_MODULE My Test 
+#include <boost/test/included/unit_test.hpp>
+
+BOOST_AUTO_TEST_CASE(first_test) 
+{
+  int i = 1;
+  BOOST_TEST(i); 
+  BOOST_TEST(i == 2); 
+}
+
+BOOST_AUTO_TEST_CASE(second_test)
+{
+  int i = 0;
+  BOOST_TEST(i == 0);
+}
